a53e045c76f5b6d383e484209a6a395f5a6ffaf1
Mustafa Abdelkawy <32801595+Abdelkawy16@users.noreply.github.com>
2024-03-02 01:14:53 +0200

    add the navbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import Providers from "./providers";
+import Navbar from "@/components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
           <Header />
 
           {/* Navbar */}
+          <Navbar />
 
           {/* SearchBox */}
 
